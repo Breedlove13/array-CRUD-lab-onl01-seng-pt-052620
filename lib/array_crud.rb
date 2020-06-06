7f6cd067bcbf16a26ffe38f8a0cbe04a85284a33
Brian Breedlove <b.breedlove@outlook.com>
2020-06-06 01:39:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  dogs_array = [ "cerberus", "luna" , "aries", "buzz"] 
+  dogs_array = [ "wow", "luna" , "aries", "buzz"] 
   cerberus_dog = dogs_array.shift
   
 end
