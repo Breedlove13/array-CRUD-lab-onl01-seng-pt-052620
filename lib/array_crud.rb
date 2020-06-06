54e5fed9c27c0e880f28987d81fd0925df04465f
Brian Breedlove <b.breedlove@outlook.com>
2020-06-06 01:09:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,23 +16,23 @@ def add_element_to_end_of_array(array, element)
  end
 
 def add_element_to_start_of_array(array, element)
-  dogs_array = [ "cernerus", "lune" , "aries", "buzz"] 
+  dogs_array = [ "cerberus", "lune" , "aries", "buzz"] 
   dogs_array.unshift ('pluto')
   
   end
 end
 
 def remove_element_from_end_of_array(array)
-  dogs_array = [ "cernerus", "lune" , "aries", "buzz"] 
+  dogs_array = [ "cerberus", "lune" , "aries", "buzz"] 
   
 end
 
 def remove_element_from_start_of_array(array)
-  dogs_array = [ "cernerus", "lune" , "aries", "buzz"] 
+  dogs_array = [ "cerberus", "lune" , "aries", "buzz"] 
 end
 
 def retrieve_element_from_index(array, index_number)
-  dogs_array = [ "cernerus", "lune" , "aries", "buzz"] 
+  dogs_array = [ "cerberus", "lune" , "aries", "buzz"] 
 end
 
 def retrieve_first_element_from_array(array)
